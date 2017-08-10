a521c62b62bdfc878a6146aca15ac25e526ad42d
FatulM <madani_amirreza@yahoo.com>
2017-08-10 22:17:14 +0430

    Little changes

--- src/main/java/QueryParser.java
+++ src/main/java/QueryParser.java
@@ -1,5 +1,15 @@
-class QueryParser {
-    static void method() throws RuntimeException {
-        throw new RuntimeException();
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings({"WeakerAccess", "SameParameterValue", "unused"})
+public class QueryParser {
+    private Map<String, List<String>> map;
+
+    public QueryParser parse(String query) {
+        return this;
+    }
+
+    public boolean contains(String key) {
+        return false;
     }
 }
--- src/test/java/QueryParserTest.java
+++ src/test/java/QueryParserTest.java
@@ -1,8 +1,20 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class QueryParserTest {
-    @Test(expected = RuntimeException.class)
-    public void methodTest() {
-        QueryParser.method();
+    private QueryParser queryParser;
+
+    @Before
+    public void setUp() throws Exception {
+        queryParser = new QueryParser();
     }
+
+    @Test
+    public void contains() throws Exception {
+        assertThat(queryParser.parse("key=value").contains("key"), is(true));
+    }
+
 }
