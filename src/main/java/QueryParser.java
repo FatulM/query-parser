ecea91337e777896fa184e77d4249a647a20633c
FatulM <madani_amirreza@yahoo.com>
2017-08-11 16:04:20 +0430

    ADDED flags and some of test cases ...

--- src/main/java/QueryParser.java
+++ src/main/java/QueryParser.java
@@ -1,15 +1,111 @@
-import java.util.List;
-import java.util.Map;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.EnumSet;
 
-@SuppressWarnings({"WeakerAccess", "SameParameterValue", "unused"})
+@SuppressWarnings("unused")
 public class QueryParser {
-    private Map<String, List<String>> map;
+    private EnumSet<Flag> flags;
 
-    public QueryParser parse(String query) {
+    QueryParser() {
+        flags = EnumSet.noneOf(Flag.class);
+    }
+
+    /**
+     * Checks a specified flag state.
+     *
+     * @param flag flag which we want to check state
+     * @return true if <tt>flag</tt> is added before
+     * @throws NullPointerException if <tt>flag</tt> is null
+     */
+    public boolean containsFlag(Flag flag) {
+        if (flag == null)
+            throw new NullPointerException("flag should not be null");
+        return flags.contains(flag);
+    }
+
+    /**
+     * Adds all <tt>flags</tt>.
+     * Should be used before {@link #parse}.
+     *
+     * @param flags the flags that you want to add
+     * @return this
+     * @throws NullPointerException if <tt>flags</tt> are null
+     */
+    public QueryParser addFlags(Flag... flags) {
+        if (flags == null)
+            throw new NullPointerException("flag should not be null");
+        for (Flag flag : flags)
+            if (flag == null)
+                throw new NullPointerException("flag should not be null");
+
+        this.flags.addAll(Arrays.asList(flags));
+
         return this;
     }
 
-    public boolean contains(String key) {
-        return false;
+    /**
+     * Removes <tt>flags</tt>
+     * Should be used before {@link #parse}.
+     *
+     * @param flags the flags that you want to remove
+     * @return this
+     * @throws NullPointerException if <tt>flags</tt> are null
+     */
+    public QueryParser removeFlags(Flag... flags) {
+        if (flags == null)
+            throw new NullPointerException("flag should not be null");
+        for (Flag flag : flags)
+            if (flag == null)
+                throw new NullPointerException("flag should not be null");
+
+        this.flags.removeAll(Arrays.asList(flags));
+
+        return this;
+    }
+
+    /**
+     * In addition to all alphanumerics and percent encoded characters,
+     * a query can legally include the following unencoded characters:
+     * / ? : @ - . _ ~ ! $ & ' ( ) * + , ; =
+     *
+     * @param query query string which is being checked
+     * @throws IllegalArgumentException when query has invalid characters
+     */
+    private void checkCharacters(String query) {
+    }
+
+    /**
+     * Checks query string structure.
+     * Key and value can be empty.
+     * And each key can contain multiple values.
+     * But your string can not have key=value1=value2,
+     * instead use key=value1&key=value2
+     *
+     * @param query query string which is being checked
+     * @throws IllegalArgumentException when query has invalid structure
+     */
+    private void checkStructure(String query) {
+    }
+
+    /**
+     * Parses query strings.
+     * You can get query String from URI by {@link URI#getQuery()}.
+     * Also note that your string should not include "?"
+     *
+     * @param query query string
+     * @return this
+     */
+    public QueryParser parse(String query) {
+        if (query == null)
+            throw new NullPointerException("query string should not be null");
+        checkCharacters(query);
+        checkStructure(query);
+        return this;
+    }
+
+    public enum Flag {
+        IGNORE_WHITE_SPACE,
+        MERGE_KEYS,
+        CONVERT_NULL
     }
 }
--- src/test/java/QueryParserTest.java
+++ src/test/java/QueryParserTest.java
@@ -1,20 +1,106 @@
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Theories.class)
 public class QueryParserTest {
-    private QueryParser queryParser;
+    @DataPoints("Query Strings With Illegal Characters")
+    public static final List<String> QUERY_STRING_WITH_ILLEGAL_CHARACTERS = Arrays.asList(
+            "?key=value", "\\", "key=value#", "key=value\n");
+    @Rule
+    public Timeout globalTimeout = new Timeout(1, TimeUnit.MINUTES);
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    private QueryParser qParser;
 
     @Before
     public void setUp() throws Exception {
-        queryParser = new QueryParser();
+        qParser = new QueryParser();
     }
 
     @Test
-    public void contains() throws Exception {
-        assertThat(queryParser.parse("key=value").contains("key"), is(true));
+    public void WhenRemoveNullArrayArgumentThenThrowsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("flag should not be null");
+        qParser.removeFlags((QueryParser.Flag[]) null);
     }
 
+    @Test
+    public void WhenAddNullArrayArgumentThenThrowsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("flag should not be null");
+        qParser.addFlags((QueryParser.Flag[]) null);
+    }
+
+    @Test
+    public void WhenAddNullFlagThenThrowsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("flag should not be null");
+        qParser.addFlags(QueryParser.Flag.CONVERT_NULL, null, QueryParser.Flag.IGNORE_WHITE_SPACE);
+    }
+
+    @Test
+    public void WhenRemoveNullFlagThenThrowsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("flag should not be null");
+        qParser.removeFlags(QueryParser.Flag.MERGE_KEYS, null);
+    }
+
+    @Test
+    public void WhenAddFlagsThenObjectContainsThem() throws Exception {
+        assertThat(qParser
+                .addFlags(QueryParser.Flag.MERGE_KEYS, QueryParser.Flag.CONVERT_NULL)
+                .containsFlag(QueryParser.Flag.CONVERT_NULL), is(true));
+    }
+
+    @Test
+    public void WhenCheckingNullFlagThenThrowsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("flag should not be null");
+        qParser.containsFlag(null);
+    }
+
+    @Test
+    public void GivenObjectWithSomeFlagsWhenRemovingSomeFlagsThenObjectDoesNotContainThem() throws Exception {
+        assertThat(qParser
+                .addFlags(QueryParser.Flag.MERGE_KEYS, QueryParser.Flag.CONVERT_NULL)
+                .removeFlags(QueryParser.Flag.IGNORE_WHITE_SPACE, QueryParser.Flag.CONVERT_NULL)
+                .containsFlag(QueryParser.Flag.CONVERT_NULL), is(false));
+    }
+
+    @Test
+    public void WhenParsingNullQueryStringsThenThrowsNullPointerException() throws Exception {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("query string should not be null");
+        qParser.parse(null);
+    }
+
+    @Theory
+    public void WhenParsingQueryStringWithInvalidCharactersThenThrowsIllegalArgumentException
+            (@FromDataPoints("Query Strings With Illegal Characters") String str) throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("query string has invalid characters");
+        qParser.parse(str);
+    }
+
+    @Test
+    public void WhenParsingBadStructuredQueryStringThenThrowsIllegalArgumentException() throws Exception {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("query string is bad structured");
+        qParser.parse("key1=value1&key2=value2=value3&key3=value4");
+    }
 }
